8ab6e2f5cec47be1a9ca38856d3de91bf6ead0cc
yl-yue <yl-yue@qq.com>
2019-08-23 16:37:40 +0800

    1. 标准与完善文档

--- docs/_sidebar.md
+++ docs/_sidebar.md
@@ -5,25 +5,33 @@
 
 * yue-library-base（必备）
 
-  * [base](base-介绍.md)
-  * [工具类](base-工具类.md)
-  * [校验](base-校验.md)
+  * [介绍](base/介绍.md)
+  * [工具类](base/工具类.md)
+  * [校验](base/校验.md)
+
+* yue-library-base-crypto
+
+  * [介绍](base/crypto/介绍.md)
 
 * yue-library-data-jdbc（推荐）
 
-  * [jdbc](data-jdbc-介绍.md)
-  * [增删改查](data-jdbc-增删改查.md)
+  * [介绍](data/jdbc/介绍.md)
+  * [增删改查](data/jdbc/增删改查.md)
 
 * yue-library-data-redis
 
-  * [redis](data-redis-介绍.md)
+  * [介绍](data/redis/介绍.md)
+
+* yue-library-pay
+
+  * [介绍](pay/介绍.md)
 
 * yue-规约
 
-  * [yue-规约](yue-规约.md)
-  * [编码规范](yue-规约-编码规范.md)
-  * [后端规约说明](yue-规约-后端规约说明.md)
-  * [接口质检标准](yue-规约-接口质检标准.md)
+  * [介绍](介绍.md)
+  * [编码规范](规约/编码规范.md)
+  * [后端规约说明](规约/后端规约说明.md)
+  * [接口质检标准](规约/接口质检标准.md)
 
 * [更多](更多.md)
 * [Changelog<sup style="color:red">(new)<sup>](changelog.md)
--- yue-library-pay/src/main/java/ai/yue/library/pay/ipo/PayOrderIPO.java
+++ yue-library-pay/src/main/java/ai/yue/library/pay/ipo/PayOrderIPO.java
@@ -4,27 +4,31 @@ import java.math.BigDecimal;
 
 import com.egzosn.pay.common.bean.TransactionType;
 
+import ai.yue.library.base.util.UUIDUtils;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 /**
- * 建造者：
- * <p>{@linkplain PayOrderBuilder#builder()}
- * <p>{@linkplain PayOrderBuilder#createPayOrderIPO()}
- * 
  * @author	孙金川
  * @since	2019年8月22日
  */
 @Data
+@ToString(callSuper = true) 
 @EqualsAndHashCode(callSuper = true)
 public class PayOrderIPO extends com.egzosn.pay.common.bean.PayOrder {
 
 	/**
 	 * 配置列表id，用于区分具体配置信息，全局唯一
 	 */
-    private Integer listId;
+	private Integer listId;
     
-    PayOrderIPO(Integer listId, String subject, String body, BigDecimal price, String outTradeNo, TransactionType transactionType) {
+	public PayOrderIPO(Integer listId, String subject, String body, BigDecimal price, TransactionType transactionType) {
+		super(subject, body, price, UUIDUtils.getOrderNo_19(), transactionType);
+		this.listId = listId;
+	}
+    
+	public PayOrderIPO(Integer listId, String subject, String body, BigDecimal price, String outTradeNo, TransactionType transactionType) {
         super(subject, body, price, outTradeNo, transactionType);
         this.listId = listId;
     }
