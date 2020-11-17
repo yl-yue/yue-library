feb573c3d3fb5249c236166fe4b7309d4b3ba490
李东霖 <937624219@qq.com>
2020-11-17 15:56:12 +0800

    回滚NetProxyDocs

--- yue-library-samples/yue-library-test/src/main/java/ai/yue/library/test/doc/example/base/NetProxyDocs.java
+++ yue-library-samples/yue-library-test/src/main/java/ai/yue/library/test/doc/example/base/NetProxyDocs.java
@@ -14,39 +14,40 @@ import org.springframework.boot.SpringApplication;
  */
 public class NetProxyDocs {
 
-    // 使用系统代理
+	// 使用系统代理
 
-    @Autowired
-    NetProxy netProxy;
+	public static void main(String[] args) throws Exception {
+		NetProxy.useSystemProxies();// 使用系统代理需要在项目启动之前进行设置
+		SpringApplication.run(TestApplication.class, args);
+	}
 
-    // 动态开启与关闭代理
-    @Autowired
-    NetProxyProperties netProxyProperties;
+	// 动态开启与关闭代理
 
-    public static void main(String[] args) throws Exception {
-        NetProxy.useSystemProxies();// 使用系统代理需要在项目启动之前进行设置
-        SpringApplication.run(TestApplication.class, args);
-    }
+	@Autowired
+	NetProxy netProxy;
 
-    // 获取代理配置
+	public void use() {
+		netProxy.httpServerEnable();
+		netProxy.httpServerClose();
+		netProxy.socksServerEnable();
+		netProxy.socksServerClose();
+		// https、ftp ...
+	}
 
-    public void use() {
-        netProxy.httpServerEnable();
-        netProxy.httpServerClose();
-        netProxy.socksServerEnable();
-        netProxy.socksServerClose();
-        // https、ftp ...
-    }
+	// 获取代理配置
 
-    public void getProxyConfig() {
-        netProxyProperties.getSocksServerHost();
-        netProxyProperties.getSocksServerPort();
-        netProxyProperties.getSocksProxyVersion();
-        netProxyProperties.getNonProxyHosts();
-        netProxyProperties.getNonProxyHostsAdditional();
-        netProxyProperties.getBasicAuthenticatorUsername();
-        netProxyProperties.getBasicAuthenticatorPassword();
-        // http、https、ftp ...
-    }
+	@Autowired
+	NetProxyProperties netProxyProperties;
+
+	public void getProxyConfig() {
+		netProxyProperties.getSocksServerHost();
+		netProxyProperties.getSocksServerPort();
+		netProxyProperties.getSocksProxyVersion();
+		netProxyProperties.getNonProxyHosts();
+		netProxyProperties.getNonProxyHostsAdditional();
+		netProxyProperties.getBasicAuthenticatorUsername();
+		netProxyProperties.getBasicAuthenticatorPassword();
+		// http、https、ftp ...
+	}
 
 }
