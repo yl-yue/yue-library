8b33e2a161b2814ff1aca9b2c99a08ac4fe859f9
yl-yue <yl-yue@qq.com>
2021-05-04 16:28:31 +0800

    中间件性能测试3

--- yue-library-samples/yue-library-test/src/main/java/ai/yue/library/test/controller/data/performance/MiddlewarePerformanceController.java
+++ yue-library-samples/yue-library-test/src/main/java/ai/yue/library/test/controller/data/performance/MiddlewarePerformanceController.java
@@ -2,23 +2,36 @@ package ai.yue.library.test.controller.data.performance;
 
 import ai.yue.library.base.view.R;
 import ai.yue.library.base.view.Result;
+import ai.yue.library.data.es.config.sql.EsSqlProperties;
 import ai.yue.library.data.jdbc.client.Db;
 import ai.yue.library.data.redis.client.Redis;
 import ai.yue.library.test.dataobject.performance.UserPerformanceDO;
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import com.alibaba.fastjson.JSONObject;
+import com.amazon.opendistroforelasticsearch.jdbc.ElasticsearchDataSource;
+import com.amazon.opendistroforelasticsearch.jdbc.config.HostnameVerificationConnectionProperty;
+import com.amazon.opendistroforelasticsearch.jdbc.config.PasswordConnectionProperty;
+import com.amazon.opendistroforelasticsearch.jdbc.config.TrustSelfSignedConnectionProperty;
+import com.amazon.opendistroforelasticsearch.jdbc.config.UserConnectionProperty;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * 中间件性能测试
@@ -33,8 +46,11 @@ public class MiddlewarePerformanceController {
 	@Autowired
 	private Db db;
 	@Autowired
+	private EsSqlProperties esSqlProperties;
+	@Autowired
 	@Qualifier("esDb")
 	private Db esDb;
+	private JdbcTemplate esJdbcTemplate;
 	@Autowired
 	private RestHighLevelClient elasticsearchClient;
 	private ElasticsearchRestTemplate elasticsearchRestTemplate;
@@ -46,8 +62,25 @@ public class MiddlewarePerformanceController {
 	private String key = "c17439cb32234963b4fd3d555089c44f";
 
 	@PostConstruct
-	private void init() {
+	private void init() throws SQLException {
 		elasticsearchRestTemplate = new ElasticsearchRestTemplate(elasticsearchClient);
+
+		String url = "jdbc:elasticsearch://" + esSqlProperties.getUrl();
+
+		ElasticsearchDataSource ds = new ElasticsearchDataSource();
+		ds.setUrl(url);
+
+		Properties properties = new Properties();
+		properties.setProperty(TrustSelfSignedConnectionProperty.KEY, String.valueOf(esSqlProperties.isTrustSelfSigned()));
+		properties.setProperty(HostnameVerificationConnectionProperty.KEY, String.valueOf(esSqlProperties.isHostnameVerification()));
+		String username = esSqlProperties.getUsername();
+		String password = esSqlProperties.getPassword();
+		if (StrUtil.isAllNotEmpty(username, password)) {
+			properties.setProperty(UserConnectionProperty.KEY, username);
+			properties.setProperty(PasswordConnectionProperty.KEY, password);
+		}
+		ds.setProperties(properties);
+		esJdbcTemplate = new JdbcTemplate(ds);
 	}
 
 	@GetMapping("/mysql")
@@ -64,6 +97,11 @@ public class MiddlewarePerformanceController {
 		paramJson.put("id", id);
 		paramJson.put("key", key);
 		return R.success(esDb.get(tableName, paramJson));
+	}
+
+	@GetMapping("/esSqlJdbc")
+	public Result<?> esSqlJdbc() {
+		return R.success(esJdbcTemplate.queryForMap("SELECT * FROM user WHERE id = ? AND key = ?", id, key));
 	}
 
 	@GetMapping("/esRest")
