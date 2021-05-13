12a41d774bfdaa90a5cb2c47c2fa6afbd5d674d2
yl-yue <yl-yue@qq.com>
2021-05-13 18:24:26 +0800

    1. 迁移actuator配置至auth模块，添加actuator配置安全 2. 新增安全规约 3. 改善项目介绍方式，优化文档结构

--- yue-library-auth-service/src/main/java/ai/yue/library/auth/service/config/AuthServiceAutoConfig.java
+++ yue-library-auth-service/src/main/java/ai/yue/library/auth/service/config/AuthServiceAutoConfig.java
@@ -1,13 +1,5 @@
 package ai.yue.library.auth.service.config;
 
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
-
 import ai.yue.library.auth.client.config.AuthClientAutoConfig;
 import ai.yue.library.auth.service.client.User;
 import ai.yue.library.auth.service.client.WxMaUser;
@@ -16,6 +8,13 @@ import ai.yue.library.auth.service.config.properties.QqProperties;
 import ai.yue.library.auth.service.config.properties.WxOpenProperties;
 import ai.yue.library.data.redis.client.Redis;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 
 /**
  * AuthService自动配置
@@ -37,5 +36,5 @@ public class AuthServiceAutoConfig {
 		log.info("【初始化配置-AuthService-User客户端】配置项：" + AuthServiceProperties.PREFIX + "。Bean：User ... 已初始化完毕。");
 		return new User();
 	}
-	
+
 }
